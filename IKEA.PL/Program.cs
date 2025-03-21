f65a37474106638cf0d94bcaaa7246620e012f98
moomen mohammmed <monm73530@gmail.com>
2025-03-21 13:49:32 +0200

    Project DEMO

--- IKEA.PL/Program.cs
+++ IKEA.PL/Program.cs
@@ -14,10 +14,10 @@ namespace IKEA.PL
             #region congigue sercies
             builder.Services.AddControllersWithViews();
 
-            builder.Services.AddDbContext<ApplicationDbCOntext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
+            //builder.Services.AddDbContext<ApplicationDbCOntext>(options =>
+            //{
+            //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            //});
 
 
             //builder.Services.AddScoped<ApplicationDbCOntext>();            
